156a1967aa0c5c483d09d7884475646e1badfb01
RBC-Mac <reso-boot@camp>
2017-07-14 17:33:14 +0900

    first commit

---
+++ note.md
@@ -0,0 +1,46 @@
+# Resonant Boot Camp Note
+## Day 1
+### Xcode shortcuts
+- `Cmd + r`:Build
+- `Cmd + 1 or 2 or ...`:simulator size change
+- `Cmd + Shift + h`:Home button
+
+### Counter app
+- create project
+- General
+    + deployment info
+        * Landscape right/Left :disabled
+
+- 見た目
+- プログラミング
+- 紐付け
+の順番
+
+AppleUI
+- 44 x 44以下のボタンはNG?
+
+- Xcode に画像を入れてみる
+    + 画像をD&D
+        * Destination を必ずチェック（コピーしてインプット）
+
+## Day 2
+### 課題
+- ガチャ
+- 最低三種類以上のものをランダムに表示
+- 背景やアイコン、画面遷移
+
+#### コンセプト
+
+## Day 3
+- 画面を追加
+    - storyboardにてViewControllerパーツをD&Dで追加
+    - Buttonを配置
+    - Buttonを`Ctrl`を押しながら次のViewControllerへドラッグ
+    - この流れをSegueという
+    - ツリーペインでサブクリック->`New File`し、`Cocoa Touch Class`を選び、`Subclass of`を`UIViewCOntroller`を選択。適当な名前でClassを作成。
+    - 追加したViewControllerの`CustomClass`に上で作成したクラスを設定する
+    - 追加したViewControllerに戻る関数(`dismiss`)を追加、ボタンに紐付け。
+- 任意のタイミングで画面遷移
+    - ViewControllerを選択し、`ctrl`キーを押しながら追加したViewへドラッグし、ViewControl自体をSegueでつなぐ
+    - SegueのIdentifierを設定
+    - 既存のViewControllerに戻るSegueする関数(`performSegue`)を設定し、
---
+++ rbcmemo.swift
@@ -0,0 +1,145 @@
+/**
+ * swift
+ */
+
+/*
+ * anntotations
+ */
+@IBOutlet
+@IBAction
+
+
+/*
+ * VALIABLE definition
+ * var VALUE_NAME: INIT_TYPE = INIT_VALUE
+ */
+//ex.
+var hoge: Int = 0
+var foo: UILabel!
+
+/*
+ * for statement
+ * for VALIABLE in ITEMS{}
+ * ITEMS: ex. 0..<100
+ *  - 0..<100 is continueing 0 to 99
+ *  - 0...100 is continueing 0 to 100
+ */
+
+// ex.
+for i in 0..<100 {
+    // process...
+}
+// not use "i"
+for _ in 0..<100 {}
+
+/*
+ * if statement
+ * if CONDITION{
+ * }else if CONDITIØN{
+ * }else{}
+ */
+
+ /*
+  * switch statement
+  * switch VALIABLE
+  * case VALUE:
+  * default:
+  */
+
+/*
+ * Array
+ * var array: [TYPE] = []
+ * array.append("hoge")
+ */
+
+/*
+ * function
+ */
+// Statements
+func FUNCTION_NAME (VALIABLE_NAME: ARGUMENT_TYPE) -> RETURN_VALUE_TYPE {
+
+}
+
+func hoge(variable: Int) -> String {
+    var returnValue
+    returnValue = (String)variable
+    return returnValue
+}
+
+/*Randmize*/
+let rand: IntU32 = arc4random_uniform(MAX_VALUE)
+
+/*
+ * ViewControllerをDismissする
+ */
+@IBAction func backView(){
+    self.dismiss(animated: true, completion: nil)
+    // animated:アニメーションの有無
+    // completion:処理完了後に実行すること
+}
+@IBAction func nextView(){
+    self.performSegue(withIdentifier: "SegueにつけたID", sender: nil)
+    //senderはほとんどの場合nilらしい
+}
+// UserDefaultds
+let ud = UserDefaults.standard
+ud.set(number, forKey: "number")
+ud.synchronize()
+
+number_2 = ud.integer(forKey: "number")
+
+
+
+
+
+
+ViewController first/second/third
+
+first -> second
+first -> third
+
+
+
+
+
+@IBOutlet var titleLbael: UILabel!
+@IBOutlet var countLabel: UILbael!
+
+countLabel.text = String(123)
+
+
+
+
+if nanika == 10 && 20
+if (nanika == 10) && (nanika == 20)
+
+
+
+var dishArray: array[String] = [aa,bb,cc,ddd]
+func dish(){
+    var number: Int = arc4random_uniform(4)
+
+    var number: Int = "123"
+
+    var number = Int(arc4random_uniform(4))
+    var number = Int("123")
+
+    label.text = dishArray[number]
+
+    switch number  {
+    case 1:
+        label/text = "カレー"
+        photoImage.image = UIImage(named: "carray")
+    case 2:
+        photoImage.image = UIImage(namde: "gohan")
+    }
+
+
+
+
+
+    case
+
+
+
+}
